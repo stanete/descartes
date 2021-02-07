cd197a23650cff5668bb00c81f6dc8ed2f1ff4a3
David Stanete <davidstanete@gmail.com>
2021-02-07 17:30:07 +0100

    Create controller test for creating an article

---
+++ src/test/kotlin/com/descartes/ArticleControllerTest.kt
@@ -0,0 +1,46 @@
+package com.descartes
+
+import com.descartes.ApiContractAssertion.Companion.assertResponse
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.extension.ExtendWith
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.cloud.contract.wiremock.AutoConfigureWireMock
+import org.springframework.test.context.junit.jupiter.SpringExtension
+import org.springframework.test.web.reactive.server.WebTestClient
+import reactor.core.publisher.Mono
+
+@SpringBootTest
+@AutoConfigureWebTestClient
+@ExtendWith(SpringExtension::class)
+@AutoConfigureWireMock(port = 8089)
+class ArticleControllerTest {
+
+    @Autowired
+    private lateinit var webTestClient: WebTestClient
+
+    @Test
+    fun `When creating a new article with a url return the scraped text in plain text`() {
+        val url = "https://stanete.com/system-design-101"
+        stub(
+            ArticleHttpStub(
+                url = url,
+                responseBody = getStubContent("scrap_article_response.html")
+            )
+        )
+
+        val body = CreateArticleRequestBody(url = url)
+        val response = webTestClient.post()
+            .uri("/articles")
+            .body(Mono.just(body), CreateArticleRequestBody::class.java)
+            .header("Content-Type", "application/json")
+            .exchange()
+            .expectStatus().isOk
+            .expectBody(String::class.java)
+            .returnResult()
+            .responseBody!!
+
+        assertResponse(response).matchesJsonFile("create_article_response.json")
+    }
+}
