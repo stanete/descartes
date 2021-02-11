c90d000a069238fa58968640c1b3559063dd559f
David Stanete <davidstanete@gmail.com>
2021-02-11 23:01:34 +0100

    Send a simple message to a queue when creating an article

--- src/main/kotlin/com/descartes/ArticleController.kt
+++ src/main/kotlin/com/descartes/ArticleController.kt
@@ -1,5 +1,6 @@
 package com.descartes
 
+import com.descartes.configuration.Rabbitmq.Companion.SCRAPE_ARTICLE
 import org.springframework.http.MediaType
 import org.springframework.http.ResponseEntity
 import org.springframework.http.ResponseEntity.ok
@@ -12,10 +13,13 @@ import reactor.kotlin.core.publisher.toMono
 
 @RestController
 @RequestMapping(produces = [MediaType.APPLICATION_JSON_VALUE])
-class ArticleController(val scrapeArticle: ScrapeArticle) {
+class ArticleController(val scrapeArticle: ScrapeArticle, val publisher: Publisher) {
 
     @PostMapping("/articles")
-    suspend fun create(@RequestBody requestBody: CreateArticleRequestBody): ResponseEntity<Mono<String>> = ok(
-        CreateArticleResponseBody(text = scrapeArticle(requestBody.url)).toJson().toMono()
-    )
+    suspend fun create(@RequestBody requestBody: CreateArticleRequestBody): ResponseEntity<Mono<String>> {
+        publisher.send(SCRAPE_ARTICLE, Message(mapOf("url" to requestBody.url)))
+        return ok(
+            CreateArticleResponseBody(text = scrapeArticle(requestBody.url)).toJson().toMono()
+        )
+    }
 }
