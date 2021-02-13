66bf8ed52ac54a0809bb589541aa7a0f4099854c
David Stanete <davidstanete@gmail.com>
2021-02-13 20:33:43 +0100

    Fix bad substitution for substring in environment variable

--- init.sh
+++ init.sh
@@ -3,6 +3,6 @@ set -e
 
 java -Djava.security.egd=file:/dev/./urandom \
       -Dserver.port=$PORT \
-      -Dspring.datasource.url="jdbc:${${DATABASE_URL}/postgres/postgresql}" \
+      -Dspring.datasource.url="jdbc:${DATABASE_URL/postgres/postgresql}" \
       -jar /app/descartes-*.jar \
       "web"
