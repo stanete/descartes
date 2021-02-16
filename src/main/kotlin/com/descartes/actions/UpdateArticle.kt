6a2fd09f107e139f320b07cbbbdaa41b3098540c
David Stanete <davidstanete@gmail.com>
2021-02-16 19:31:39 +0100

    Create and update article to the database

--- src/main/kotlin/com/descartes/actions/UpdateArticle.kt
+++ src/main/kotlin/com/descartes/actions/UpdateArticle.kt
@@ -1,14 +1,15 @@
 package com.descartes.actions
 
 import com.descartes.articles.Article
+import com.descartes.articles.ArticleRepository
 import org.springframework.stereotype.Service
 import java.util.logging.Logger
 
 @Service
-class UpdateArticle {
+class UpdateArticle(private val articleRepository: ArticleRepository) {
 
     operator fun invoke(article: Article): Article {
-        Logger.getGlobal().info("Updating article $article")
-        return article
+        Logger.getGlobal().info("Updating article $article.")
+        return articleRepository.save(article)
     }
 }
