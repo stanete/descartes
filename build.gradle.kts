8faf2f5a76bc6ef8ef3785fba2ad2220ba03b3fb
David Stanete <davidstanete@gmail.com>
2021-02-21 02:58:40 +0100

    Add Text Razor library dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,13 +1,13 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-	id("org.springframework.boot") version "2.4.2"
-	id("io.spring.dependency-management") version "1.0.11.RELEASE"
-	id("io.gitlab.arturbosch.detekt") version "1.15.0"
-	id("org.flywaydb.flyway") version "6.0.7"
-	kotlin("jvm") version "1.4.30"
-	kotlin("plugin.spring") version "1.4.30"
-	kotlin("plugin.jpa") version "1.4.21"
+    id("org.springframework.boot") version "2.4.2"
+    id("io.spring.dependency-management") version "1.0.11.RELEASE"
+    id("io.gitlab.arturbosch.detekt") version "1.15.0"
+    id("org.flywaydb.flyway") version "6.0.7"
+    kotlin("jvm") version "1.4.30"
+    kotlin("plugin.spring") version "1.4.30"
+    kotlin("plugin.jpa") version "1.4.21"
 }
 
 apply(plugin = "org.flywaydb.flyway")
@@ -17,15 +17,15 @@ version = "0.0.1-SNAPSHOT"
 java.sourceCompatibility = JavaVersion.VERSION_13
 
 repositories {
-	mavenCentral()
-	jcenter {
-		content {
-			// just allow to include kotlinx projects
-			// detekt needs 'kotlinx-html' for the html report
-			includeGroup("org.jetbrains.kotlinx")
-			includeGroup("org.amshove.kluent")
-		}
-	}
+    mavenCentral()
+    jcenter {
+        content {
+            // just allow to include kotlinx projects
+            // detekt needs 'kotlinx-html' for the html report
+            includeGroup("org.jetbrains.kotlinx")
+            includeGroup("org.amshove.kluent")
+        }
+    }
 }
 
 val detektVersion = "1.15.0"
@@ -34,58 +34,60 @@ val wiremockVersion = "3.0.1"
 val mockkVersion = "1.10.6"
 val kotlinResultVersion = "1.1.11"
 val kluentVersion = "1.65"
+val textRazorVersion = "1.0.9"
 
 dependencies {
-	implementation("org.springframework.boot:spring-boot-starter-actuator")
-	implementation("org.springframework.boot:spring-boot-starter-webflux")
-	implementation("org.springframework.boot:spring-boot-starter-amqp")
-	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
-	implementation("org.flywaydb:flyway-core")
-	runtimeOnly("org.postgresql:postgresql")
-	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
-	implementation("io.projectreactor.kotlin:reactor-kotlin-extensions")
-	implementation("org.jetbrains.kotlin:kotlin-reflect")
-	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
-	implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor")
-	implementation("it.skrape:skrapeit-core:$skrapeItVersion")
-	implementation("com.michael-bull.kotlin-result:kotlin-result:$kotlinResultVersion")
-	testImplementation("org.springframework.boot:spring-boot-starter-test")
-	testImplementation("io.projectreactor:reactor-test")
-	testImplementation("io.mockk:mockk:$mockkVersion")
-	testImplementation("org.springframework.cloud:spring-cloud-contract-wiremock:$wiremockVersion")
-	testImplementation("org.amshove.kluent:kluent:$kluentVersion")
-	testImplementation("org.springframework.amqp:spring-rabbit-test")
-	detekt("io.gitlab.arturbosch.detekt:detekt-formatting:$detektVersion")
-	detekt("io.gitlab.arturbosch.detekt:detekt-cli:$detektVersion")
+    implementation("org.springframework.boot:spring-boot-starter-actuator")
+    implementation("org.springframework.boot:spring-boot-starter-webflux")
+    implementation("org.springframework.boot:spring-boot-starter-amqp")
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    implementation("org.flywaydb:flyway-core")
+    runtimeOnly("org.postgresql:postgresql")
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+    implementation("io.projectreactor.kotlin:reactor-kotlin-extensions")
+    implementation("org.jetbrains.kotlin:kotlin-reflect")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor")
+    implementation("it.skrape:skrapeit-core:$skrapeItVersion")
+    implementation("com.michael-bull.kotlin-result:kotlin-result:$kotlinResultVersion")
+    implementation("com.textrazor:textrazor:$textRazorVersion")
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testImplementation("io.projectreactor:reactor-test")
+    testImplementation("io.mockk:mockk:$mockkVersion")
+    testImplementation("org.springframework.cloud:spring-cloud-contract-wiremock:$wiremockVersion")
+    testImplementation("org.amshove.kluent:kluent:$kluentVersion")
+    testImplementation("org.springframework.amqp:spring-rabbit-test")
+    detekt("io.gitlab.arturbosch.detekt:detekt-formatting:$detektVersion")
+    detekt("io.gitlab.arturbosch.detekt:detekt-cli:$detektVersion")
 }
 
 detekt {
-	toolVersion = detektVersion
-	input = files("./src")
-	config = files("./detekt-config.yml")
-	autoCorrect = true
+    toolVersion = detektVersion
+    input = files("./src")
+    config = files("./detekt-config.yml")
+    autoCorrect = true
 }
 
 tasks.register<Exec>("flywayCreate") {
-	group = "flyway"
-	description =
-		"Creates a new migration by using the difference between JPA model in your current branch and model " +
-			"created by migrations in main branch."
-	run {
-		standardInput = System.`in`
-		standardOutput = System.`out`
-	}
-	workingDir("./db/scripts/")
-	commandLine("./createMigration.sh")
+    group = "flyway"
+    description =
+        "Creates a new migration by using the difference between JPA model in your current branch and model " +
+            "created by migrations in main branch."
+    run {
+        standardInput = System.`in`
+        standardOutput = System.`out`
+    }
+    workingDir("./db/scripts/")
+    commandLine("./createMigration.sh")
 }
 
 tasks.withType<KotlinCompile> {
-	kotlinOptions {
-		freeCompilerArgs = listOf("-Xjsr305=strict")
-		jvmTarget = "11"
-	}
+    kotlinOptions {
+        freeCompilerArgs = listOf("-Xjsr305=strict")
+        jvmTarget = "11"
+    }
 }
 
 tasks.withType<Test> {
-	useJUnitPlatform()
+    useJUnitPlatform()
 }
